5292ce351a6bc339625b1b1cd19d4d1d39040248
maida <mehdi.aida.drakin@gmail.com>
2020-08-02 15:03:59 +0200

    a lot of stuff in this commit

--- api.js
+++ api.js
@@ -3,7 +3,12 @@ const app = express();
 const stream = require('stream');
 const ytdl = require('ytdl-core')
 const fs = require('fs');
+var cors = require('cors');
+var compression = require('compression')
 
+
+app.use(cors());
+app.use(compression())
 
 app.listen(4000, () => {
  console.log("Server running on port 4000");
@@ -14,13 +19,14 @@ app.get("/download", (req, res, next) => {
     var videoId=req.query.videoId;
     var type=req.query.type;
     var title=req.query.title;
+    var url = "https://www.youtube.com/watch?v=" + videoId
 
-    var dl=(type == 'video') ? ytdl(url, {filter: format => format.container === 'mp4'}):ytdl(url,{ filter:'audioonly'})
-    var wstream=fs.createWriteStream('./files/'+ title + '.mp4')
-    dl.pipe(wstream)
+    console.log(url)
 
-    wstream.on('finish', () => {
-        console.log('All writes are now complete.');
-        res.download('/files/' + title + '.mp4');
-    });
+    var filename=(type == 'video')?title + '.mp4':title+ '.mp3'
+    var dl=(type == 'video') ? ytdl(url, {filter: format => format.container === 'mp4'}):ytdl(url,{ filter:'audioonly',  dlChunkSize: '256KB'})
+   
+    res.attachment(filename)
+    var pass = new stream.PassThrough();
+    dl.pipe(pass).pipe(res)
 });
